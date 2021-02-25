82d16ebd5df86f15d9fbee3ef3228c4970f5cb42
Olga-Kulagina <olga.kulagina17@yandex.ru>
2021-02-25 12:46:56 +0300

    refactor api

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,10 +3,10 @@ import axios from 'axios'
 export const tableAPI = {
     //Маленький объем данных
     getSmallTable() {
-        return axios.get('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
+        return axios.get('https://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D')
     },
     //Большой объем данных
     getBigTable() {
-        return axios.get('http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
+        return axios.get('https://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}')
     }
 }
